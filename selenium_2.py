ee913d6c7bf865523f240324e2c8fcd3d282e29b
Hyewon Park <65341841+study-mid@users.noreply.github.com>
2021-11-29 16:42:47 +0900

    4열 '품목 카테고리' 추가

--- selenium_2.py
+++ selenium_2.py
@@ -9,9 +9,9 @@ import pandas as pd
 # step2.크롬드라이버로 원하는 url로 접속(해당 사용자의 url 그때마다 입력)
 url = 'https://www.daangn.com/u/상세주소'
 # 틀린 주소 예
-#'https://www.daangn.com/u/상세주소?install_from=user_profile'
+# 'https://www.daangn.com/u/상세주소?install_from=user_profile'
 # 맞는 주소 예
-#'https://www.daangn.com/u/상세주소'
+# 'https://www.daangn.com/u/상세주소'
 driver = webdriver.Chrome('C:/Users/chromedriver.exe')
 driver.get(url)
 time.sleep(3)
@@ -20,7 +20,6 @@ time.sleep(3)
 user = driver.find_element_by_id("nickname").text
 user = user.split(' ', 1)[0]
 region_name = driver.find_element_by_id("region_name").text
-
 
 # 판매 물품 개수 파악 ------------------------------------------------------------------------------ #
 # 계속해서 스크롤 다운하면서 데이터를 다 조회하는 코드 : https://hello-bryan.tistory.com/194
@@ -54,7 +53,6 @@ z = int(img_nums % 18)
 print(y + 1)
 
 data = []  # 크롤링한 데이터를 [제목, 본문] 형식으로 저장할 딕셔너리
-data.append([user, region_name])
 
 # 숨김 처리된 글 탐지용
 hide_article = False
@@ -87,6 +85,8 @@ for n in range(1, y + 2):
             hide_article = False
         else:
             item_titles = driver.find_element_by_id("article-title").text
+            item_category = driver.find_element_by_id("article-category").text
+            item_category = item_category.split(' ', 1)[0]
             item_details = driver.find_elements_by_id("article-detail")
             item_links = driver.current_url
 
@@ -95,7 +95,10 @@ for n in range(1, y + 2):
                 i = i.text
             detail = "".join(i)
             # list1 = [item_titles, detail, item_links]
-            list1 = ['', '', item_titles, detail]
+            if n == 1 and m == 1:
+                list1 = [user, region_name, item_titles, item_category, detail]
+            else:
+                list1 = ['', '', item_titles, item_category, detail]
             data.append(list1)
             print(detail)
             print("({0}/{1})\n--------------------------".format(current_num, img_nums))
@@ -105,9 +108,9 @@ for n in range(1, y + 2):
             break
         driver.back()
 
-
 # data 리스트 엑셀 파일로 저장
-data = pd.DataFrame(data) # 데이터 프레임으로 전환
-data.to_csv('C:/파일경로/{0}.csv'.format(user), index=False, header=['닉네임', '사는 동네', '판매 품목', '판매 내용'], encoding="utf-8-sig")
+data = pd.DataFrame(data)  # 데이터 프레임으로 전환
+data.to_csv('C:/파일경로/{0}_카테고리추가.csv'.format(user), index=False,
+            header=['닉네임', '사는 동네', '판매 품목', '품목 카테고리', '판매 내용'], encoding="utf-8-sig")
 
 driver.quit()
